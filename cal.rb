e7bb03928b3b75d8e73bd594860df256c3576479
Murray Catto <murraycatto@live.co.za>
2023-11-30 17:40:56 +0000

    Add dynamic month and begin to add dynamic days

--- cal.rb
+++ cal.rb
@@ -10,22 +10,26 @@ end
 require "date"
 
 module Calendar
-  def self.name_of_month
-    Date.today.strftime("%B %Y")
+  def self.name_of_month(year: Date.today.year, month: Date.today.month)
+    "   #{Date.new(year,month).strftime("%B %Y")}"
   end
 
   def self.days_of_week
     "Su Mo Tu We Th Fr Sa"
   end
 
-  def self.days_of_month
+  def self.days_of_month(year: Date.today.year, month: Date.today.month)
     <<~CAL
                 1  2  3  4
        5  6  7  8  9 10 11
       12 13 14 15 16 17 18
       19 20 21 22 23 24 25
-      26 27 28 29 30
+      26 27 28 29 \e[31m30\e[0m
     CAL
+  end
+
+  def self.first_line_of_days_of_month(day: 0)
+    " 1  2  3  4  5  6  7"
   end
 end
 
@@ -33,7 +37,7 @@ require "rspec/autorun"
 
 RSpec.describe "calendar" do
   it "prints the name of the month" do
-    expect(Calendar.name_of_month).to eq "November 2023"
+    expect(Calendar.name_of_month).to eq "   November 2023"
   end
 
   it "prints the days of the week" do
@@ -46,7 +50,36 @@ RSpec.describe "calendar" do
        5  6  7  8  9 10 11
       12 13 14 15 16 17 18
       19 20 21 22 23 24 25
-      26 27 28 29 30
+      26 27 28 29 \e[31m30\e[0m
     CAL
   end
+
+  it "prints the month for the year and month passed in" do
+    expect(Calendar.name_of_month(year: 2020, month: 12)).to eq "   December 2020"
+  end
+
+  it "prints the days for the year and month passed it" do
+    expect(Calendar.days_of_month(year: 2023, month: 12)).to eq(<<~CAL)
+                      1  2
+       3  4  5  6  7  8  9
+      10 11 12 13 14 15 16
+      17 18 19 20 21 22 23
+      24 25 26 27 28 29 30
+      31
+    CAL
+  end
+
+  it "returns rest of the days of the week including whitespace before hand" do
+    expect(Calendar.first_line_of_days_of_month(day: 1)).to eq(" 1  2  3  4  5  6  7")
+    expect(Calendar.first_line_of_days_of_month(day: 2)).to eq("    1  2  3  4  5  6")
+    expect(Calendar.first_line_of_days_of_month(day: 3)).to eq("       1  2  3  4  5")
+    expect(Calendar.first_line_of_days_of_month(day: 4)).to eq("          1  2  3  4")
+    expect(Calendar.first_line_of_days_of_month(day: 5)).to eq("             1  2  3")
+    expect(Calendar.first_line_of_days_of_month(day: 6)).to eq("                1  2")
+    expect(Calendar.first_line_of_days_of_month(day: 7)).to eq("                   1")
+  end
 end
+
+puts Calendar.name_of_month
+puts Calendar.days_of_week
+puts Calendar.days_of_month
