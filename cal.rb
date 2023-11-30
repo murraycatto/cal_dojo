8ac05f7eea04ff9bf1af385b95ae640cc3426903
Tom Chipchase <tom.chipchase@easol.com>
2023-11-30 18:17:19 +0000

    WIP

--- cal.rb
+++ cal.rb
@@ -5,9 +5,12 @@ require "bundler/inline"
 gemfile do
   source "https://rubygems.org"
   gem "rspec"
+  gem "activesupport"
+  gem "pry"
 end
 
 require "date"
+require "active_support/all"
 
 module Calendar
   def self.name_of_month(year: Date.today.year, month: Date.today.month)
@@ -19,8 +22,12 @@ module Calendar
   end
 
   def self.days_of_month(year: Date.today.year, month: Date.today.month)
+    wday = Date.new(year, month).beginning_of_month.wday
+
+    puts wday
+
     <<~CAL
-                1  2  3  4
+      #{first_line_of_days_of_month(day: wday)}
        5  6  7  8  9 10 11
       12 13 14 15 16 17 18
       19 20 21 22 23 24 25
@@ -29,7 +36,25 @@ module Calendar
   end
 
   def self.first_line_of_days_of_month(day: 0)
-    " 1  2  3  4  5  6  7"
+    case day
+    when 0
+      " 1  2  3  4  5  6  7"
+    when 1
+      "    1  2  3  4  5  6" when 2
+      "       1  2  3  4  5"
+    when 3
+      "          1  2  3  4"
+    when 4
+      "             1  2  3"
+    when 5
+      "                1  2"
+    when 6
+      "                   1"
+    end
+  end
+
+  def self.other_line_of_month(start: 0)
+    (start..).take(7).map { |a| a.to_s.chars.count == 1 ? "  #{a}" : " #{a}" }.join.lstrip
   end
 end
 
@@ -44,7 +69,7 @@ RSpec.describe "calendar" do
     expect(Calendar.days_of_week).to eq "Su Mo Tu We Th Fr Sa"
   end
 
-  it "prints the days of the month" do
+  it "prints the days of the month", :focus do
     expect(Calendar.days_of_month).to eq(<<~CAL)
                 1  2  3  4
        5  6  7  8  9 10 11
@@ -77,6 +102,11 @@ RSpec.describe "calendar" do
     expect(Calendar.first_line_of_days_of_month(day: 5)).to eq("             1  2  3")
     expect(Calendar.first_line_of_days_of_month(day: 6)).to eq("                1  2")
     expect(Calendar.first_line_of_days_of_month(day: 7)).to eq("                   1")
+  end
+
+  it "returns 7 days from the passed in date" do
+    expect(Calendar.other_line_of_month(start: 2)).to eq("2  3  4  5  6  7  8")
+    expect(Calendar.other_line_of_month(start: 4)).to eq("4  5  6  7  8  9 10")
   end
 end
 
